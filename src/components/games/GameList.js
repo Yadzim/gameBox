54637f252b9284fdfde8fc451e69fb337072091c
Eric <82119304+Yadzim@users.noreply.github.com>
2021-12-03 10:31:15 +0500

    v

---
+++ src/components/games/GameList.js
@@ -0,0 +1,53 @@
+// styles
+import styles from "./GameList.module.css";
+import GameItem from "./GameItem";
+import styled from "styled-components";
+import { useState } from "react";
+
+const GameList = ({ items }) => {
+  const [count, setCount] = useState(15);
+
+  const getCount=()=>{
+    setCount(100);
+  }
+
+  return (
+    <div>
+    <div className={styles.grid}>
+      {items.map((game, i) => i<=count&&(
+        <GameItem key={game.id} item={game} />
+      ))}
+    </div>
+    {count!==100 &&(
+    <div className={styles.grid} >
+    <div></div>
+    <div></div>
+    <div></div>
+      <Button onClick={()=>getCount()}>All game</Button>
+    </div>)}
+    </div>
+  );
+};
+
+export default GameList;
+
+const Button = styled.div`
+  height: 50px;
+  border-radius: 5px;
+  background: rgba(255,255,255,.05);
+  margin-top: 20px;
+  margin-bottom: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  transition: all .2s  ease-out;
+  &:hover{
+    box-shadow: 3px 5px 18px rgba(0, 0, 0, 0.6);
+  }
+  &:active{
+    box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.1);
+  }
+`;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router-dom";
+import { IoGridOutline } from "react-icons/io5";
+// styles
+import styles from "./Header.module.css";
+
+const Header = () => {
+  const activeStyle = {
+    color: "orange",
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.container} container`}>
+        <NavLink to="/" style={{display:"flex", alignItems:"center"}}>
+          <IoGridOutline className={styles.logo} style={{ color: 'orange'}} />
+          <span style={{marginLeft:10, color:"orange", fontSize:24}}>GameBox</span>
+        </NavLink>
+        <nav className={styles.navigation}>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="popular"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="favorites"
+                style={({ isActive }) => (isActive ? activeStyle : undefined)}
+              >
+                Favorites
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
